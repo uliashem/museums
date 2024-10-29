606ac341379e7b704afe72f36b8b27b4f0051432
uliashem <shemiakinau@gmail.com>
2024-10-29 12:27:40 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [museums, setMuseums] = useState(data);
+  const [showText, setShowText] = useState(false);
+
+  const showTextClick = (museum) => {
+    museum.showMore = !museum.showMore;
+    setShowText(!showText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>The Best-{museums.length} Museums of London </h1>
+      </div>
+
+      {museums.map ((museum => {
+        const {id, museumName, image, description, address, hours, showMore} = museum;
+
+        return (
+          <div>  
+
+            <div className='container'>
+              <div className='container1'>
+                <h2>{id}. {museumName}</h2>
+              </div>
+
+              <div className='container1'>
+                <p>{showMore ? description : description.substring(0, 190) + "..."}
+                <button onClick={() => showTextClick(museum)}>{showMore ? "Show less" : "Show more"}</button>
+                </p>
+                
+              </div>
+
+            <div className='container1'>
+              <img src={image} width="100%" alt='Museums'/>
+            </div>
+
+            <div className='container1'>
+              <p><span>Address:</span> {address}</p>
+            </div>
+
+            <div className='container1'>
+              <p><span>The museum is open</span> {hours}</p>
+            </div>
+
+            </div>
+
+          </div>
+        )
+      }))}
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const data = [
+
+  {
+    id: 1,
+    museumName: "British Museum",
+    image: "https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2022/01/25144008/summer-of-culture-british-museum-1024x606-1.jpg",
+    description: "The domed glass-and-steel ceiling at the British Museum – the world’s first national museum opened to the public – lets light into the atrium-like Great Court below, bouncing off the blinding-white, sweeping staircases and marble walls. It’s one of the capital’s prettiest spaces, but beyond the impressive lobby, it has sprawling galleries dedicated to the Egyptians, the Greeks and the Romans. Artefacts including the Rosetta Stone, Pantheon Marbles (once a part of the original structure in Athens) and more than 120 mummies draw millions of visitors every year. This is the city’s most-visited attraction, and it’s easy to see why.",
+    address: "Great Russell Street, London, WC1B 3DG",
+    hours: "daily from 10am-5pm",
+    showMore: false
+  }, 
+
+  {
+    id: 2,
+    museumName: "Natural History Museum",
+    image: "https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2020/04/18073914/natural-history-museum-virtual-tour-788x524.jpg",
+    description: "The ornate façade of the Natural History Museum is a love letter to Victorian architecture: a vast gothic building that looks more like a cathedral than a museum. Inside the lofty Hintze Hall (revamped in 2017 to strip it back to its 19th-century splendour), Hope, a ginormous blue-whale skeleton, hangs above the information desk, which doubles up as a bar when the museum throws late-night sessions. See dinosaur fossils, the first edition of Charles Darwin’s On the Origin of Species and casts from Pompeii among the museum’s 80 million specimens.",
+    address: "Cromwell Road, South Kensington, London, SW7 5BD",
+    hours: "daily from 10am-5.50pm",
+    showMore: false
+  }, 
+
+  {
+    id: 3,
+    museumName: "The Science Museum",
+    image: "https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2022/02/25143550/shutterstock_1362438632.jpg",
+    description: "Another free museum in South Kensington, dedicated to the miracle of technological progress… and all the neat things we’ve discovered along the way. Some of the ‘classic’ galleries at The Science Museum focusing on the birth of steam power are a little fusty, but the bit about space travel is neat, and there are tons of neat interactive exhibitions to play with. The Wonderlab area (which isn’t free alas) is full of hands-on experiments that kids will go absolutely bananas for. ",
+    address: "Exhibition Road, South Kensington, London, SW7 2DD",
+    hours: "Wednesday to Sunday from 10am-6pm",
+    showMore: false
+  }, 
+
+  {
+    id: 4,
+    museumName: "Victoria & Albert Museum",
+    image: "https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2022/01/25144122/VA-Museum-1024x642.jpeg",
+    description: "London’s largest art museum – the eighth-biggest art museum in the world – has more than 100 galleries displaying everything from photography to jewellery. It has put on some of the city’s most talked-about exhibitions in recent years: the curators have become known for their hot-ticket fashion displays of designers, including Christian Dior and Alexander McQueen. Make sure to visit the permanent Rapid Response Collection, which has contemporary pieces that react to significant moments in recent history, such as the 'pussyhats' worn at protests against the election of Donald Trump to a 3D-printed Extinction Rebellion coin.",
+    address: "Cromwell Road, London, SW7 2RL",
+    hours: "Monday to Sunday from 10am-5.45pm",
+    showMore: false
+  }, 
+
+  {
+    id: 5,
+    museumName: "Imperial War Museum",
+    image: "https://offloadmedia.feverup.com/secretldn.com/wp-content/uploads/2018/10/18120441/Imperial-War-Museum-5-840x1120.jpg",
+    description: "Housed in what was once the site of Bethlem Royal Hospital (the psychiatric facility better known as Bedlam) and flanked by naval guns, the Imperial War Museum explores the role of British troops in conflicts that cross generations and countries. It’s full of often challenging exhibitions, from displays on World War I to the Holocaust.",
+    address: "Lambeth Road, London, SE1 6HZ",
+    hours: "daily from 10am-6pm",
+    showMore: false
+  }
+
+]
